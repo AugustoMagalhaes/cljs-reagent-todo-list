6effeb074111210fe5c772f78929da47d619786c
AugustoMagalhaes <augustommg@gmail.com>
2022-11-14 12:23:31 -0300

    feat: linka cor a booleano :completed

--- src/cljs_reagent_todo/core.cljs
+++ src/cljs_reagent_todo/core.cljs
@@ -8,14 +8,14 @@
             []))
 
 (defn todo-item [todo]
-  [:li {:style {:color "green"} :key (str todo)}(:desc todo)])
+  [:li {:style {:color (if (:completed todo) "green" "red")} :key (str todo)}(:desc todo)])
 
 (defn todo-form []
   (let [novo-item (r/atom "")]
     (fn []
       [:form {:on-submit (fn [e]
                            (.preventDefault e)
-                           (swap! todos conj {:color "green" :desc @novo-item :key @novo-item})
+                           (swap! todos conj {:completed false :desc @novo-item :key @novo-item})
                            (reset! novo-item ""))}
        [:input {:type "text"
                 :value @novo-item
