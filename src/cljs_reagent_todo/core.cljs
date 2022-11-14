08650be6e25960e2a55b3f2e75238b34c640109d
AugustoMagalhaes <augustommg@gmail.com>
2022-11-14 12:30:17 -0300

    feat: adiciona logica do checkbox novo-item-completado

--- src/cljs_reagent_todo/core.cljs
+++ src/cljs_reagent_todo/core.cljs
@@ -11,12 +11,15 @@
   [:li {:style {:color (if (:completed todo) "green" "red")} :key (str todo)}(:desc todo)])
 
 (defn todo-form []
-  (let [novo-item (r/atom "")]
+  (let [novo-item (r/atom "")
+        novo-item-completado (r/atom false)]
     (fn []
       [:form {:on-submit (fn [e]
                            (.preventDefault e)
-                           (swap! todos conj {:completed false :desc @novo-item :key @novo-item})
-                           (reset! novo-item ""))}
+                           (swap! todos conj {:completed @novo-item-completado :desc @novo-item :key @novo-item})
+                           (reset! novo-item "")
+                           (reset! novo-item-completado false))}
+       [:input {:type "checkbox" :value @novo-item-completado :checked @novo-item-completado :on-change #(reset! novo-item-completado (-> % .-target .-checked))}]
        [:input {:type "text"
                 :value @novo-item
                 :placeholder "Adicione um novo item"
